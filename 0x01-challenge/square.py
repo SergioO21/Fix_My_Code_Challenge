af3d045225eb835f4689589f52b389f555866147
Sergio Orejarena <sergioorueda@gmail.com>
2021-09-20 01:15:44 -0500

    Is it a Square?

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,13 +2,9 @@
 
 class square():
     
-    width = 0
-    height = 0
-
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width, height):
+        self.width = width
+        self.height = width
 
     def area_of_my_square(self):
         """ Area of the square """
